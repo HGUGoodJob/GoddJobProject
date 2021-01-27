7cbf63ccfccc81e63f48b3ef6e803a56d59260ba
haram <21801034@handong.edu>
2021-01-27 16:20:14 +0900

    노래고르기 추가

--- app/src/main/java/com/goodjob/singing/ChoiceSong.java
+++ app/src/main/java/com/goodjob/singing/ChoiceSong.java
@@ -7,19 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class ChoiceLevel extends AppCompatActivity {
+public class ChoiceSong extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choice_level);
+        setContentView(R.layout.activity_choice_song);
 
-        ImageButton click_start = (ImageButton)findViewById(R.id.levellow);
+        ImageButton click_start = (ImageButton)findViewById(R.id.song_plane);
         click_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (
-                        getApplicationContext(), ChoiceSong.class);
+                        getApplicationContext(), PartPractice.class);
                 startActivity(intent);
             }
         });
