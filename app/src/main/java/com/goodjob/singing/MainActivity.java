13dc99daf93018941481a4c7b997b68a62671a05
DaheeHan <21700765@handong.edu>
2021-01-30 13:27:35 +0900

    intro 화면 애니메이션 효과 넣기

--- app/src/main/java/com/goodjob/singing/MainActivity.java
+++ app/src/main/java/com/goodjob/singing/MainActivity.java
@@ -6,19 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      //  Animation ani = AnimationUtils.LoadA
+
         Button click_start = (Button)findViewById(R.id.button1);
+
         click_start.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/goodjob/singing/SplashActivity.java
+++ app/src/main/java/com/goodjob/singing/SplashActivity.java
@@ -8,11 +8,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 public class SplashActivity extends AppCompatActivity {
-    VideoView v;
-
+    //VideoView v;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,17 +23,25 @@ public class SplashActivity extends AppCompatActivity {
 //                WindowManager.LayoutParams.FLAG_FULLSCREEN); //상태바 없애는 코
         setContentView(R.layout.activity_splash);
 
-        v = findViewById(R.id.splash);
-        Uri videoUri;
-        videoUri = Uri.parse("android.resource://"+getPackageName()+ "/" + R.raw.mainsplash);
-        v.setVideoURI(videoUri);
 
-        v.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                v.start();
-            }
-        });
+
+        //v = findViewById(R.id.splash);
+        tv = findViewById(R.id.intro);
+        Animation ani = AnimationUtils.loadAnimation(this, R.anim.intro);
+
+        tv.startAnimation(AnimationUtils.loadAnimation(this, R.anim.intro));
+
+
+//        Uri videoUri;
+//        videoUri = Uri.parse("android.resource://"+getPackageName()+ "/" + R.raw.mainsplash);
+        //v.setVideoURI(videoUri);
+
+//        v.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//            @Override
+//            public void onPrepared(MediaPlayer mp) {
+//                v.start();
+//            }
+//        });
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
