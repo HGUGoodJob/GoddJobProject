c96bf6773a969b44607cc12bad58968338932f2c
haram <21801034@handong.edu>
2021-02-17 00:27:51 +0900

    권한 설정 팝업창 뜨기

--- app/src/main/java/com/goodjob/singing/MainActivity.java
+++ app/src/main/java/com/goodjob/singing/MainActivity.java
@@ -1,30 +1,72 @@
 package com.goodjob.singing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] permission_list = {
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.VIBRATE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        checkPermission();
 
-        Button click_start = (Button)findViewById(R.id.button1);
+        Button click_start = (Button) findViewById(R.id.button1);
 
         click_start.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (
+                Intent intent = new Intent(
                         getApplicationContext(), ChoiceLevel.class);
                 startActivity(intent);
             }
         });
+    }
+
+    public void checkPermission() {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return;
+
+        for (String permission : permission_list) {
+            int check = checkCallingOrSelfPermission(permission);
+            if (check == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(permission_list, 0);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==0)
+        {
+            for(int i=0; i<grantResults.length; i++)
+            {
+                if(grantResults[i]==PackageManager.PERMISSION_GRANTED){
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"앱 권한 설정이 필요합니다.",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        }
     }
 }
